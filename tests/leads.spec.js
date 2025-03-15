32a8337bd70c2bd3c6fdf53e8111493aae10ee75
Jhonata Amboni <jhonata01amboni@gmail.com>
2025-03-15 19:46:55 -0300

    interação com elementos temporarios

--- tests/leads.spec.js
+++ tests/leads.spec.js
@@ -29,6 +29,17 @@ test('deve cadastrar um lead na fila de espera', async ({ page }) => {
   //await page.getByText('Quero entrar na fila!').click();
 
   await page.getByTestId('modal').getByText('Quero entrar na fila!').click();
-  
+
+  await page.getByText('Agradecemos por compartilhar seus dados conosco. Em breve, nossa equipe entrará em contato!').click();
+  //busca o html do elemento que clicou na linha de cima
+  const content = await page.content();
+  console.log(content);//print no console da UI do playwright, dai la da de pegar o class .toast usado abaixo
+
+  await expect(
+    page.locator('.toast')
+  ).toHaveText('Agradecemos por compartilhar seus dados conosco. Em breve, nossa equipe entrará em contato!');  
+
+  //aguarda o elemento .toast ficar invisivel em até dois segundos
+  await expect(page.locator('.toast')).toBeHidden({timeout: 5000});
   await page.waitForTimeout(5000);
 });
